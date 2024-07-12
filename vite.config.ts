cc39d1fbb9977201f2d5a6d6d31d47e2cfa61a06
unknown <edesbalazs@gmail.com>
2024-07-12 12:35:15 +0200

    debugging alphatab folder

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,23 @@ import { defineConfig } from 'vite'
 import { alphaTab } from '@coderline/alphatab/vite'
 import react from '@vitejs/plugin-react'
 
+import { readdirSync } from 'fs'
+import { resolve } from 'path'
+
+const dist = readdirSync(resolve('node_modules/@coderline/alphatab/dist'))
+console.log('dist:')
+console.log(dist)
+
+const font = readdirSync(resolve('node_modules/@coderline/alphatab/dist/font'))
+console.log('font:')
+console.log(font)
+
+const sf = readdirSync(
+  resolve('node_modules/@coderline/alphatab/dist/soundfont'),
+)
+console.log('soundfont:')
+console.log(sf)
+
 export default defineConfig({
   plugins: [react(), alphaTab()],
   appType: 'spa',
