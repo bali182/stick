9b0dfdcd1955d2b52285b4ad8dc5d3ae23b5a77c
unknown <edesbalazs@gmail.com>
2024-08-16 15:41:59 +0200

    Added down 1 transitions

--- generated2/transitions/down1.ts
+++ generated2/transitions/down1.ts
@@ -1,10 +1,67 @@
 import { Transition, TransitionCategory2 } from '../../src/model/types'
 
+export const fromAnyToMinus1RootRootFifthUpRoot: Transition = {
+  id: 'S:?;T:-1;C,R->C,R->C,F,U->C,R',
+  name: 'R R 5\u2191 R',
+  steps: [
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+    { chordRef: 'CURRENT', dir: 'UP', interval: 0, tone: 'FIFTH' },
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+  ],
+  tags: [],
+  target: -1,
+}
+
+export const fromAnyToMinus1RootRootFifthDownRoot: Transition = {
+  id: 'S:?;T:-1;C,R->C,R->C,F,D->C,R',
+  name: 'R R 5\u2193 R',
+  steps: [
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+    { chordRef: 'CURRENT', dir: 'DOWN', interval: 0, tone: 'FIFTH' },
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+  ],
+  tags: [],
+  target: -1,
+}
+
+export const fromAnyToMinus1RootRootThirdUpRoot: Transition = {
+  id: 'S:?;T:-1;C,R->C,R->C,T,U->C,R',
+  name: 'R R 3\u2191 R',
+  steps: [
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+    { chordRef: 'CURRENT', dir: 'UP', interval: 0, tone: 'THIRD' },
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+  ],
+  tags: [],
+  target: -1,
+}
+
+export const fromAnyToMinus1RootThirdUpFifthUpRoot: Transition = {
+  id: 'S:?;T:-1;C,R->C,T,U->C,F,U->C,R',
+  name: 'R 3\u2191 5\u2191 R',
+  steps: [
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+    { chordRef: 'CURRENT', dir: 'UP', interval: 0, tone: 'THIRD' },
+    { chordRef: 'CURRENT', dir: 'UP', interval: 0, tone: 'FIFTH' },
+    { chordRef: 'CURRENT', dir: 'NONE', interval: 0, tone: 'ROOT' },
+  ],
+  tags: [],
+  target: -1,
+}
+
 export const down1: TransitionCategory2 = {
   id: 'down1-down1',
   name: 'From any chord down 1 semitones',
   variableName: 'down1',
   sourceFileName: 'down1',
   tags: [],
-  transitions: [],
+  transitions: [
+    fromAnyToMinus1RootRootFifthUpRoot,
+    fromAnyToMinus1RootRootFifthDownRoot,
+    fromAnyToMinus1RootRootThirdUpRoot,
+    fromAnyToMinus1RootThirdUpFifthUpRoot,
+  ],
 }
--- transitions2/down1.ts
+++ transitions2/down1.ts
@@ -1,6 +1,11 @@
 import { RawTransitionCategory2 } from '../src/model/types'
 
 export const down1: RawTransitionCategory2 = {
-  transitions: [],
+  transitions: [
+    'C,R -> C,R -> C,F,U -> C,R',
+    'C,R -> C,R -> C,F,D -> C,R',
+    'C,R -> C,R -> C,T,U -> C,R',
+    'C,R -> C,T,U -> C,F,U -> C,R',
+  ],
   target: -1,
 }
